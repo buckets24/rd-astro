2e48c5de79787d97cb54ba4e7a36673935f9017a
buckets24 <carlo.buckets@gmail.com>
2025-03-13 15:51:12 +0800

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown({ config: { debug: true, forward: ['dataLayer.push'] } })]
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        debug: true,
+        forward: ['dataLayer.push']
+      }
+    })
+  ]
 });
